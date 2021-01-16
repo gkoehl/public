be39dba0741fdc4b27c72507b688770888a832a7
Gary Koehl <gkoehl.slalom@gmail.com>
2021-01-16 13:14:54 -0600

    Updating

--- scripts/workstation/init.cmd
+++ scripts/workstation/init.cmd
@@ -10,21 +10,12 @@ readonly UNAME_OUT="$(uname -s)"
 echo ${UNAME_OUT}
 case "${UNAME_OUT}" in
   Linux*)
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-linux.sh)"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-filesystem.sh)"
-    # RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${VERSION_AWS_CLI}.zip" -o "awscliv2.zip" \
-    #   && ls -lh awscliv2.zip \
-    #   && unzip awscliv2.zip \
-    #   && ./aws/install \
-    #   && aws --version
-    # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
-    # unzip awscliv2.zip
-    # sudo ./aws/install
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/master/scripts/workstation/init-linux.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/master/scripts/workstation/init-filesystem.sh)"
   ;;
   Darwin*)
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-mac.sh)"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-filesystem.sh)"
-    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-cli.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/master/scripts/workstation/init-mac.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/master/scripts/workstation/init-filesystem.sh)"
   ;;
   CYGWIN*)
   ;;
