c02c1cf2911cb97e66bbe0cd9ab38fb6615be807
Gary Koehl <gkoehl.slalom@gmail.com>
2021-01-16 15:39:14 -0600

    Updating.

--- scripts/workstation/init-cli.sh
+++ scripts/workstation/init-cli.sh
@@ -5,6 +5,21 @@ set -o pipefail
 ################################################################################
 # CLI
 ################################################################################
+# RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${VERSION_AWS_CLI}.zip" -o "awscliv2.zip" \
+#   && ls -lh awscliv2.zip \
+#   && unzip awscliv2.zip \
+#   && ./aws/install \
+#   && aws --version
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+
+# git clone <repo>
+# cd <repo>
+# asdf install
+# pipenv install -e .
+# pipenv run <package>
+# cd ${OLDPWD}
 
 ################################################################################
 # EXIT
