1811e8e499effcf76e623c9f748e428b85f5f29c
Gary Koehl <gkoehl.slalom@gmail.com>
2021-01-16 13:09:07 -0600

    Updating

--- scripts/workstation/init.cmd
+++ scripts/workstation/init.cmd
@@ -1,25 +1,25 @@
 echo off
 
-# notes:
-# - run using:
-#   `sh -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init.cmd)"`
-
 goto() {
 # Linux
+# notes:
+# - run using:
+#   `sh -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/master/scripts/workstation/init.cmd)"`
+
 readonly UNAME_OUT="$(uname -s)"
 echo ${UNAME_OUT}
 case "${UNAME_OUT}" in
   Linux*)
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-linux.sh)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-filesystem.sh)"
-    @REM RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${VERSION_AWS_CLI}.zip" -o "awscliv2.zip" \
-    @REM   && ls -lh awscliv2.zip \
-    @REM   && unzip awscliv2.zip \
-    @REM   && ./aws/install \
-    @REM   && aws --version
-    @REM curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
-    @REM unzip awscliv2.zip
-    @REM sudo ./aws/install
+    # RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${VERSION_AWS_CLI}.zip" -o "awscliv2.zip" \
+    #   && ls -lh awscliv2.zip \
+    #   && unzip awscliv2.zip \
+    #   && ./aws/install \
+    #   && aws --version
+    # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+    # unzip awscliv2.zip
+    # sudo ./aws/install
   ;;
   Darwin*)
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/gkoehl/public/scripts/workstation/master/init-mac.sh)"
